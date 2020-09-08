3d4e150f0eff3d8d497a9fbd6ef50988e081530d
afolabiopakunle <opakunleafolabi@gmail.com>
2020-09-08 07:13:35 +0100

    auto time delay, scroll and load  effects added

--- delete-pending-posts-on-facebook.js
+++ delete-pending-posts-on-facebook.js
@@ -1,10 +1,9 @@
-//ENSURE YOU ARE LOGGED IN AS AN THE GROUP'S MODERATOR OR ADMIN AND NAVIGATE TO THE PENDING POSTS PAGE
-//PRESS CTRL + SHIFT + I TO OPEN THE BROWSER CONSOLE AND PASTE THESE FOLLOWING CODES LINE BY LINE
-
-let deleteButtons = document.querySelectorAll('.sx_f437f9'); deleteButtons.forEach(btn => btn.click());
-//select all delete buttons on screen, bot clicks all the selected delete buttons
-
-let del = document.querySelectorAll('.layerConfirm'); del.forEach(btn => btn.click());
-//this line selects all delete confirmation that appears on screen. Bot automatically clicks and delete on all the modal pop-ups on screen
-
-//Refresh the page and run the 
+setInterval(function () {
+    let deleteButtons = document.querySelectorAll('.sx_1e2b64'); //selection of the delete button
+    deleteButtons.forEach(btn => btn.click());
+    setInterval(function () {
+        let del = document.querySelectorAll('.layerConfirm');
+        del.forEach(btn => btn.click());
+    }, 5000)
+    window.scrollBy(0, 10000)
+}, 15000)
